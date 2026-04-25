1d70ace697874dd54f6bf9582c94121f97f86986
Igor <igorshk271@gmail.com>
2026-04-25 16:00:57 +0300

    jwt-auth guard created

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,19 @@ import { UsersModule } from "./modules/users/users.module";
 import { RoomsModule } from "./modules/rooms/rooms.module";
 import { AuthModule } from "./modules/auth/auth.module";
 import { WalletsModule } from "./modules/wallets/wallets.module";
+import {ConfigModule} from "@nestjs/config";
 
 @Module({
-  imports: [DatabaseModule, UsersModule, RoomsModule, AuthModule, WalletsModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    DatabaseModule,
+    UsersModule,
+    RoomsModule,
+    AuthModule,
+    WalletsModule,
+  ],
   controllers: [],
   providers: [],
 })
--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -5,17 +5,23 @@ import { TypeOrmModule } from "@nestjs/typeorm";
 import { User } from "../users/entities/user.entity";
 import { Role } from "../users/entities/role.entity";
 import { JwtModule } from "@nestjs/jwt";
+import {JwtStrategy} from "./strategy/jwt.strategy";
+import {ConfigModule, ConfigService} from "@nestjs/config";
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([User, Role]),
-    JwtModule.register({
-      secret: process.env.JWT_SECRET ?? "dev-secret",
-      signOptions: { expiresIn: "2h" },
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        secret: config.get<string>('JWT_SECRET'),
+        signOptions: { expiresIn: '2h' },
+      }),
     }),
   ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
   exports: [AuthService, JwtModule],
 })
 export class AuthModule {}
